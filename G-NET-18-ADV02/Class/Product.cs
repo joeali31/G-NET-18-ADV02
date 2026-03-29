057ec105cd9772aa3a5dd48b3c327ce29ddfed44
joeali31 <ya9415565@gmail.com>
2026-03-29 16:13:44 -0700

    Task01

---
+++ G-NET-18-ADV02/Class/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_18_ADV02.Class
+{
+    public class Product
+    {
+        public Product(int id, string name, string category, double price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+            Price = price;
+            Stock = stock;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
--- G-NET-18-ADV02/Program.cs
+++ G-NET-18-ADV02/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace G_NET_18_ADV02
+﻿using G_NET_18_ADV02.Class;
+
+namespace G_NET_18_ADV02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Task01
+            List<Product> products = new List<Product>()
+            {
+                new Product(1 , "labtop" , "Elctronics" , 20000 , 10),
+                new Product(2 , "Phone" , "Elctronics" , 7000 , 5),
+                new Product(3 , "T-shirt" , "Clothing" , 500 , 15),
+                new Product(4 , "Jeans" , "Clothing" , 900 , 100),
+                new Product(5 , "Chocolate" , "Food" , 30 , 10),
+                new Product(6 , "novel" , "Books" , 200 , 11),
+                new Product(7 , "Jacket" , "Clothing" , 650, 104),
+            };
+
+            //List<Product> res = SearchProducts(products, p => p.Category == "Elctronics"); 
+            #endregion
+
         }
+
+        static List<Product> SearchProducts(List<Product> products , Func<Product, bool> ProductExists)
+        {
+            List<Product> res = [];
+
+            foreach (var item in products)
+            {
+                if (ProductExists(item))
+                {
+                    res.Add(item);
+                }
+            }
+
+            return res;
+        }
+
     }
 }
